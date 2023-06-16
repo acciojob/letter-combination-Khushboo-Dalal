efb3169265716745457342dde7bff9cf789e6a67
Khushboo Dalal <khushboodalal1434@gmail.com>
2023-06-16 13:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function letterCombinations(input_digit) {
   //Complete the function
 	let table = {
-		"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", "6" : "mno", 
-		"7" : "pqrs", "8" : "tuv", "9" : "wxyz"
+		"2" : "abc",
+		"3" : "def",
+		"4" : "ghi",
+		"5" : "jkl",
+		"6" : "mno", 
+		"7" : "pqrs",
+		"8" : "tuv",
+		"9" : "wxyz"
 	}
 
 	let solutions = [] ;
